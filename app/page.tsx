ef32d8501f93fe69f65f7a5da1d0d3b7cceb9976
unkfex-rgb <unkfex@gmail.com>
2026-01-20 22:45:20 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 
-import Header from "../components/Header";
-import PlayerGrid from "../components/PlayerGrid";
+"use client";
+
+import { useState } from "react";
+import Header from "@/components/Header";
+import StreamGrid from "@/components/StreamGrid";
+import CinemaMode from "@/components/CinemaMode";
 
 export default function Home() {
+  const [cinema, setCinema] = useState<string | null>(null);
+
   return (
     <>
+      <div className="background" />
+
       <Header />
-      <PlayerGrid />
+
+      <main className="container">
+        <StreamGrid onCinema={setCinema} />
+      </main>
+
+      {cinema && <CinemaMode url={cinema} onClose={() => setCinema(null)} />}
     </>
   );
 }
