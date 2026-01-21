f71bb944d8997f774c990b5d46e0a02aada7482f
unkfex-rgb <unkfex@gmail.com>
2026-01-20 22:19:41 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+import Header from "../components/Header";
+import PlayerGrid from "../components/PlayerGrid";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <PlayerGrid />
+    </>
+  );
+}
